4a4a4ed0b9d77fa094a21a1199407d93db1f1a78
alexander-amaral <amaralal@bc.edu>
2023-09-22 11:38:58 -0400

    Update purchases.py

---
+++ purchases.py
@@ -0,0 +1,35 @@
+purchase_number = int(input("Number of purchases: "))
+
+tax = float(input("Sales tax: "))
+
+customer_list = []
+cost_list = []
+new_cost_list = []
+
+while purchase_number > 0:
+    customer = input("Customer: ")
+    customer_list.append(customer) 
+    cost = float(input("Cost: "))
+    cost_list.append(cost)
+    purchase_number -= 1
+
+def add_tax(cost_list,tax):
+    for cost in cost_list:
+        new_cost = cost*(1+tax)
+        round(new_cost, 1)
+        new_cost_list.append(new_cost)
+    return new_cost_list
+        
+add_tax(cost_list, tax)
+
+purchases = {}
+
+for x in range(len(customer_list)):
+    customer = customer_list[x]
+    new_cost = new_cost_list[x]
+    purchases[customer] = new_cost
+
+print(purchases)
+
+ 
+
